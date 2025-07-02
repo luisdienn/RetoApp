ee9b96fa18abf0dd71afa6f6dde9263b9dc251fb
luisdienn <ldienb@ucenfotec.ac.cr>
2025-07-02 15:03:56 -0600

    Initial setup with Shakapacker and Tailwind

---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-bold">Hello from React + Tailwind!</h1>
+      <p className="mt-2">This component is styled with Tailwind CSS</p>
+    </div>
+  )
+}
+
+export default HelloWorld
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get "home/index"
   get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
